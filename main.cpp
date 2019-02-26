8e365c76ae1bf2f0085f17ed2b4e8823539763f8
wanjun <wanjuncode@163.com>
2019-02-26 13:39:29 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -16,19 +16,30 @@ void mosquitto_test(){
     }else{
         printf("libarary init successful\n");
         mosquitto *client = mosquitto_new("wanjun",true,NULL);
-        // 设置 遗言
-        int err = mosquitto_will_set(client,"wanjun_topic",strlen("hello"),"hello",1,true);
-        if(err == MOSQ_ERR_SUCCESS){
-            printf("mosquitto will set successful\n");
-        }
-
         if(client == NULL){
             goto fail;
         }else{
             printf("mosquitto new successful\n");
-
-
         }
+
+        const char *will = "hello this is will";
+        // 设置 遗言
+        int err = mosquitto_will_set(client,"wanjun_topic",strlen(will),will,1,false);
+        if(err == MOSQ_ERR_SUCCESS){
+            printf("mosquitto will set successful\n");
+        }
+        // 设置 账号 密码
+        // mosquitto_username_pw_set
+        err = mosquitto_connect(client,"localhost",1883,60);
+        if(err == MOSQ_ERR_SUCCESS){
+            printf("mosquitto connect successful\n");
+        }
+
+        int messageId = 0;
+        const char *message = "hello world";
+        mosquitto_publish(client,&messageId,"wanjun_topic",strlen(message),message,1,true);
+
+        mosquitto_disconnect(client);
         mosquitto_destroy(client);
         mosquitto_lib_cleanup();
         return;
